1e80a7d1df983b782e28192658fdb153a227a3c5
Thomaz Reis <thor27@gmail.com>
2012-12-08 09:07:02 -0200

    Fix scripts to improve package quality

--- build-hacked.sh
+++ build-hacked.sh
@@ -1,8 +1,11 @@
-
+cd `dirname $0`
+user=`whoami`
 sed -i 's#steam-login#steam-login-hacked#g' steam-login/DEBIAN/control
 mkdir -p steam-login/etc
 > steam-login/etc/steam-hacked
-dpkg --build steam-login .
+sudo chown -R root:root .
+sudo dpkg --build steam-login .
+sudo chown -R $user:$user .
 rm -r steam-login/etc
 sed -i 's#steam-login-hacked#steam-login#g' steam-login/DEBIAN/control
 
--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-dpkg --build steam-login .
+cd `dirname $0`
+user=`whoami`
+sudo chown -R root:root .
+sudo dpkg --build steam-login .
+sudo chown -R $user:$user .
